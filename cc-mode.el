048e555fbdf1d80e7239932e199bb7f60eb21055
warsaw <warsaw>
1992-03-31 23:04:10 +0000

    added organization

--- cc-mode.el
+++ cc-mode.el
@@ -1,5 +1,5 @@
 ;; C++ code editing commands for Emacs
-;; 1992 Barry Warsaw (bwarsaw@cen.com)
+;; 1992 Barry Warsaw, Century Computing Inc. (bwarsaw@cen.com)
 ;; 1987 Dave Detlefs  (dld@cs.cmu.edu) 
 ;; and  Stewart Clamen (clamen@cs.cmu.edu).
 ;; Done by fairly faithful modification of:
