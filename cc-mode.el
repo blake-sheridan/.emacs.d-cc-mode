745d968cfa607372b00fd3436b3973d74679ea4f
warsaw <warsaw>
1992-08-27 16:41:30 +0000

    added GNU copyright info

--- cc-mode.el
+++ cc-mode.el
@@ -1,7 +1,4 @@
-;; -*- Mode: Emacs-Lisp -*-
 ;;; c++-mode.el --- major mode for editing C++ (and C) code
-
-;; Copyright (C) 1992 Free Software Foundation, Inc.
 
 ;; Author: 1992 Barry A. Warsaw, Century Computing Inc. <baw@cen.com>
 ;;         1987 Dave Detlefs and Stewart Clamen
@@ -11,6 +8,24 @@
 ;; Version:         $Revision$
 ;; Last Modified:   $Date$
 ;; Keywords: C++ C editing major-mode
+
+;; Copyright (C) 1992 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;; Introduction
 ;; ============
