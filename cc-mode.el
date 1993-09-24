f8125e0fe5bb3d12227a6184fea35e5e8bf83807
warsaw <warsaw>
1993-09-24 22:05:21 +0000

    (c++-indent-exp): when inside a class definition, if we are looking at an open brace, we apply c-continued-brace-offset instead of just zero. this allows users to indent the opening brace of an inline function body.

--- cc-mode.el
+++ cc-mode.el
@@ -1387,7 +1387,8 @@ of the expression are preserved."
 	      (setq this-indent
 		    (+ this-indent c-brace-offset
 		       (if (c++-at-top-level-p t (car contain-stack))
-			   0 c-indent-level))))
+			   c-continued-brace-offset
+			 c-indent-level))))
 	     ;; check for continued statements
 	     ((save-excursion
 		(c++-backward-syntactic-ws (car contain-stack))
