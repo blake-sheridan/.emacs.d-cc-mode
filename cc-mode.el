679456ca6d7a51f649fbc65ccd2cf7090f8fe36e
warsaw <warsaw>
1994-06-23 17:54:49 +0000

    (c-inside-bracelist-p): when finding an enum, check to be sure its not a type descriptor on a return value of a func (i.e. skip-chars-forward for not semi or open paren).

--- cc-mode.el
+++ cc-mode.el
@@ -2747,7 +2747,7 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
      (if (and (< (point) containing-sexp)
 	      (looking-at "\\(typedef[ \t]+\\)?enum[ \t\n]+")
 	      (save-excursion
-		(skip-chars-forward "^;" containing-sexp)
+		(skip-chars-forward "^;(" containing-sexp)
 		(= (point) containing-sexp)))
 	 (point)))
    ;; this will pick up array/aggregate init lists, even if they are nested.
