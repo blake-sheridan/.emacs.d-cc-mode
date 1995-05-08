18d0bfce5706f7f0f44468ed343276dcc60c0aa9
bwarsaw <bwarsaw>
1995-05-08 19:42:48 +0000

    (c-in-literal): Faster implementation for skipping over cpp macros, at the expense of prohibiting `#' characters as first non-whitespace char on a line inside another literal (i.e. string or block comment).

--- cc-mode.el
+++ cc-mode.el
@@ -2894,27 +2894,29 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 ;; is used as the backward limit of the search.  If omitted, or nil,
 ;; `beginning-of-defun' is used."
 
-;; This is for all v19 Emacsen supporting either 1-bit or 8-bit syntax
+;; This is a faster version of c-in-literal.  It trades speed for one
+;; approximation, namely that within other literals, the `#' character
+;; cannot be the first non-whitespace on a line.
 (defun c-in-literal (&optional lim)
-  ;; Determine if point is in a C++ literal. we cache the last point
-  ;; calculated if the cache is enabled
+  ;; first check the cache
   (if (and (boundp 'c-in-literal-cache)
 	   c-in-literal-cache
 	   (= (point) (aref c-in-literal-cache 0)))
       (aref c-in-literal-cache 1)
-    (let ((rtn (save-excursion
-		 (let* ((lim (or lim (c-point 'bod)))
-			(here (point))
-			(state (parse-partial-sexp lim (point))))
-		   (cond
-		    ((nth 3 state) 'string)
-		    ((nth 4 state) (if (nth 7 state) 'c++ 'c))
-		    ((progn
-		       (goto-char here)
-		       (beginning-of-line)
-		       (looking-at "[ \t]*#"))
-		     'pound)
-		    (t nil))))))
+    ;; quickly check for cpp macro. this breaks if the `#' character
+    ;; appears as the first non-whitespace on a line inside another
+    ;; literal.
+    (let* (state
+	   (rtn (cond
+		 ((= (char-after (c-point 'boi)) ?#)
+		  'pound)
+		 ((nth 3 (setq state (save-excursion
+				       (parse-partial-sexp
+					(or lim (c-point 'bod))
+					(point)))))
+		  'string)
+		 ((nth 4 state) (if (nth 7 state) 'c++ 'c))
+		 (t nil))))
       ;; cache this result if the cache is enabled
       (and (boundp 'c-in-literal-cache)
 	   (setq c-in-literal-cache (vector (point) rtn)))
