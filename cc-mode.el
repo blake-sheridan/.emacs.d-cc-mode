93f9db6be045427baa4f808f8d4f22c5a4e96d7c
bwarsaw <bwarsaw>
1996-12-11 00:34:43 +0000

    From RMS: I made this change to improve the menu bar menu.

--- cc-mode.el
+++ cc-mode.el
@@ -842,6 +842,10 @@ All other Emacsen use the `old-re' suite.")
 	(define-key map [menu-bar] (make-sparse-keymap))
 	(define-key map [menu-bar c] (cons name (make-sparse-keymap name)))
 
+	(define-key map [menu-bar c compile]
+	  '("Compile" . compile)) 
+	(define-key map [menu-bar c separator1]
+	  '("----")) 
 	(define-key map [menu-bar c comment-region]
 	  '("Comment Out Region" . comment-region))
 	(define-key map [menu-bar c c-macro-expand]
@@ -854,6 +858,8 @@ All other Emacsen use the `old-re' suite.")
 	  '("Indent Line" . c-indent-command))
 	(define-key map [menu-bar c fill]
 	  '("Fill Comment Paragraph" . c-fill-paragraph))
+	(define-key map [menu-bar c separator2]
+	  '("----")) 
 	(define-key map [menu-bar c up]
 	  '("Up Conditional" . c-up-conditional))
 	(define-key map [menu-bar c backward]
