be2fcaf8528644dc3e217e0f1f3af5374c30071f
mast <mast>
1998-03-11 20:11:57 +0000

    #Removed the cc-mode-19 require kludge since it will be executed from #cc-defs anyway.

--- cc-mode.el
+++ cc-mode.el
@@ -80,20 +80,8 @@
 ;;; Code:
 
 
-(eval-when-compile
-  (require 'cc-make))
-
-(require 'cc-defs)
-
-;; sigh.  give in to the pressure, but make really sure all the
-;; definitions we need are here
-(if (or (not (fboundp 'functionp))
-	(not (fboundp 'char-before))
-	(not (c-safe (char-after) t))
-	(not (fboundp 'when))
-	(not (fboundp 'unless)))
-    (require 'cc-mode-19))
-
+(eval-and-compile
+  (require 'cc-defs))
 (require 'cc-menus)
 (require 'cc-vars)
 (require 'cc-engine)
@@ -404,6 +392,9 @@ Key bindings:
 		   'c-label-minimum-indentation
 		   'defun-prompt-regexp
 		   'tab-width
+		   ;; A brain-damaged XEmacs only variable that, if
+		   ;; set to nil can cause all kinds of chaos.
+		   'signal-error-on-buffer-boundary
 		   )))
 	(if (not (boundp 'defun-prompt-regexp))
 	    (delq 'defun-prompt-regexp vars)
